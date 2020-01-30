7c5b33632b5a8934c18e449814cab5a6a2fa0325
Kristian Mandrup <kmandrup@gmail.com>
2020-01-30 10:44:45 +0000

    add key for row

--- src/sortable-table-body.js
+++ src/sortable-table-body.js
@@ -1,27 +1,23 @@
-import { Component, PropTypes } from 'react';
+import { Component, PropTypes } from "react";
 
 class SortableTableRow extends Component {
   render() {
-    var tds = this.props.columns.map(function (item, index) {
-      var value = this.props.data[item.key];
-      if ( item.render ) {
-        value = item.render(value)
-      }
-      return (
-        <td
-          key={index}
-          style={item.dataStyle}
-          {...(item.dataProps || {})} >
-          {value}
-        </td>
-      );
-    }.bind(this));
-
-    return (
-      <tr>
-        {tds}
-      </tr>
+    const { rowKey, item, data, columns } = this.props;
+    var tds = columns.map(
+      function(item, index) {
+        var value = data[item.key];
+        if (item.render) {
+          value = item.render(value);
+        }
+        return (
+          <td key={index} style={item.dataStyle} {...(item.dataProps || {})}>
+            {value}
+          </td>
+        );
+      }.bind(this)
     );
+
+    return <tr key={item[rowKey || "id"]}>{tds}</tr>;
   }
 }
 
@@ -29,23 +25,26 @@ export default class SortableTableBody extends Component {
   static propTypes = {
     data: PropTypes.array.isRequired,
     columns: PropTypes.array.isRequired,
-    sortings: PropTypes.array.isRequired
-  }
+    sortings: PropTypes.array.isRequired,
+    rowKey: PropTypes.string,
+    item: PropTypes.object
+  };
 
   render() {
-    var bodies = this.props.data.map(((item, index) => {
-      return (
-        <SortableTableRow
-          key={index}
-          data={item}
-          columns={this.props.columns} />
-      );
-    }).bind(this));
-
-    return (
-      <tbody>
-        {bodies}
-      </tbody>
+    var bodies = this.props.data.map(
+      ((item, index) => {
+        return (
+          <SortableTableRow
+            key={index}
+            item={item}
+            rowKey={this.props.rowKey}
+            data={item}
+            columns={this.props.columns}
+          />
+        );
+      }).bind(this)
     );
+
+    return <tbody>{bodies}</tbody>;
   }
 }
